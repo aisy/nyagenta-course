e9a90d386b202bb9c33ecf96ea89c19dc62ef655
aisy <aisymuhammad@gmail.com>
2020-09-03 18:34:13 +0700

    add new page Courses

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from "../pages/Home";
+import Home from '../pages/Home';
+import Course from '../pages/Courses';
 
 const Router = (props) => {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/courses"} component={Course} />
     </Switch>
   )
 }
