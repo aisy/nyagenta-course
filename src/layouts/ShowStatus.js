8b425e871a5ee1c689dd31379fa77f8a67238c55
aisy <aisymuhammad@gmail.com>
2020-09-04 14:59:40 +0700

    add new Layout

---
+++ src/layouts/ShowStatus.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../components/commons/Navbar';
+
+const LayoutHome = (props) => {
+  return (
+    <>
+      <div>
+        {props.children}
+      </div>
+    </>
+  )
+}
+
+export default LayoutHome;
