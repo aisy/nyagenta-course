9a43b5586aeb36ce08f09debc660df1ac314a419
aisy <aisymuhammad@gmail.com>
2020-09-04 07:03:47 +0700

    add link for each card

--- src/components/Home/CardListCourse.js
+++ src/components/Home/CardListCourse.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button } from 'antd';
+import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 
 const CardListCourse = ({ title, description, img, width }) => {
@@ -8,18 +8,20 @@ const CardListCourse = ({ title, description, img, width }) => {
 
   return (
     <div>
-      <Card
-        hoverable
-        style={{ width: 290, marginBottom: 20 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      // actions={[
-      //   <Link to={"/"}>
-      //     <Button type={"primary"} shape={"round"}>Lihat semua</Button>
-      //   </Link>
-      // ]}
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+      <Link to={"/"}>
+        <Card
+          hoverable
+          style={{ width: 290, marginBottom: 20 }}
+          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        // actions={[
+        //   <Link to={"/"}>
+        //     <Button type={"primary"} shape={"round"}>Lihat semua</Button>
+        //   </Link>
+        // ]}
+        >
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
+      </Link>
     </div>
   )
 }
