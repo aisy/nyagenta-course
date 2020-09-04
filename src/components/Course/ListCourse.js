2c8efe5bb930b14edffc782745a673c0646eed09
aisy <aisymuhammad@gmail.com>
2020-09-04 07:05:42 +0700

    add new component in Course

---
+++ src/components/Course/ListCourse.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+import CardListCourse from '../Home/CardListCourse';
+
+const ListCourse = ({ data }) => {
+
+  return (
+    <div>
+      <Row gutter={16}>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+        <Col className={"gutter-row"} span={6}>
+          <CardListCourse />
+        </Col>
+      </Row>
+
+
+    </div>
+  )
+}
+
+export default ListCourse
