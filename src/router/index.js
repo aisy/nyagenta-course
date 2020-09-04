1b14aa92a0a83177eb625effb295bf2021ea2ac4
aisy <aisymuhammad@gmail.com>
2020-09-04 15:00:48 +0700

    add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home';
-import Course from '../pages/Courses';
+import Course from '../pages/Courses/';
+import Blog from '../pages/Blog';
+import NotFound from '../pages/NotFound';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const Router = (props) => {
   return (
     <Switch>
       <Route path={"/"} exact component={Home} />
+      <Route path={"/login"} component={Login} />
+      <Route path={"/register"} component={Register} />
 
       {/* Courses */}
       <Route path={"/courses"} exact component={Course} />
+
+      {/* Blog */}
+      <Route path={"/blog"} exact component={Blog} />
+
+      {/* NoutFound */}
+      <Route component={NotFound} />
     </Switch>
   )
 }
