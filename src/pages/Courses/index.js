d9b6d5e375b9065e0a2a20e9b86b672e25af9360
aisy <aisymuhammad@gmail.com>
2020-09-08 00:35:05 +0700

    add function to manage service and implement with real data

--- src/pages/Courses/index.js
+++ src/pages/Courses/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BackTop } from 'antd';
+
+import { getData } from '../../services/sample'
 
 import LayoutCourse from '../../layouts/Default';
 import Section from '../../components/commons/Section';
@@ -7,13 +9,36 @@ import SearchBar from '../../components/Course/SearchBarCourse';
 import ListCourse from '../../components/Course/ListCourse';
 
 const Courses = () => {
+
+  const [sample, setSample] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const resGetData = async () => {
+      setLoading(true);
+      try {
+        let data = await getData();
+        setSample(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.warn(error);
+      }
+    }
+
+    resGetData();
+  }, []);
+
   return (
     <>
       <LayoutCourse>
         <BackTop />
         <Section>
           <SearchBar />
-          <ListCourse />
+          <ListCourse
+            data={sample}
+            loading={loading}
+          />
         </Section>
       </LayoutCourse>
     </>
